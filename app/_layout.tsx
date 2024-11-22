416fa7cedf72677d458f744d493b404de59b9ad4
thoriq.noer <thoriq.noer@gmail.com>
2024-11-22 15:13:06 +0700

    prepare for react native papaer dates

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,5 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { NativeBaseProvider } from 'native-base';
 import { ApolloProvider } from '@apollo/client';
 import { useFonts } from 'expo-font';
 import { Stack, Slot } from 'expo-router';
@@ -7,11 +8,13 @@ import { useEffect } from 'react';
 import client from '../ApolloClient';
 import 'react-native-reanimated';
 import { AuthContextProvider } from '../context';
-import Page from '.';
+import { en, registerTranslation } from 'react-native-paper-dates';
 
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
+
+registerTranslation('en', en)
 
 export default function RootLayout() {
   const [loaded] = useFonts({
@@ -35,12 +38,14 @@ export default function RootLayout() {
   return (
     <ApolloProvider client={client}>
       <AuthContextProvider>
-        <Stack>
-          <Stack.Screen name="index" options={{ headerShown: false, }} />
-          <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-          <Stack.Screen name="(root)" options={{ headerShown: false }} />
-          <Stack.Screen name="+not-found" />
-        </Stack>
+        <NativeBaseProvider>
+          <Stack>
+            <Stack.Screen name="index" options={{ headerShown: false, }} />
+            <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+            <Stack.Screen name="(root)" options={{ headerShown: false }} />
+            <Stack.Screen name="+not-found" />
+          </Stack>
+        </NativeBaseProvider>
       </AuthContextProvider>
     </ApolloProvider>
 
