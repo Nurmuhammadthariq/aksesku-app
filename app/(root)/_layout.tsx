4deadfe7c765ab2a81ea8c12fc5b96ce28c0e6a3
thoriq.noer <thoriq.noer@gmail.com>
2024-11-22 15:11:08 +0700

    Ignore warning in react 19, SSRProvider

--- app/(root)/_layout.tsx
+++ app/(root)/_layout.tsx
@@ -1,6 +1,11 @@
 import { Stack } from "expo-router";
+import { LogBox } from "react-native"
+import { useEffect } from "react";
 
 const Layout = () => {
+    useEffect(() => {
+        LogBox.ignoreLogs(["In React 18, SSRProvider is not necessary and is a noop. You can remove it from your app."])
+    }, [])
     return (
         <Stack>
             <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
